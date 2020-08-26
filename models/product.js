49f409ad37c93676399aeb68258fa06a9a09fc25
vanshg395 <smartstart4vg@gmail.com>
2020-08-26 22:43:11 +0530

    model done

--- models/product.js
+++ models/product.js
@@ -1,4 +1,21 @@
-const products = [];
+const fs = require("fs");
+const path = require("path");
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  "data",
+  "products.json"
+);
+
+const getProductsFromFile = (cb) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
 
 module.exports = class Prodcut {
   constructor(t) {
@@ -6,10 +23,16 @@ module.exports = class Prodcut {
   }
 
   save() {
-    products.push(this);
+    // products.push(this);
+    getProductsFromFile((products) => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
   }
 
-  static fetchAll() {
-    return products;
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
   }
 };
